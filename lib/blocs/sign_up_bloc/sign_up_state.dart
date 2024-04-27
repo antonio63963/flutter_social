e8c99c44013edd63c02cf3ecc7f6818beb5af90b
antonio63963 <shotvideo25@gmail.com>
2024-04-27 12:01:24 +0300

    singUp bloc

--- lib/blocs/sign_up_bloc/sign_up_state.dart
+++ lib/blocs/sign_up_bloc/sign_up_state.dart
@@ -1,6 +1,14 @@
 part of 'sign_up_bloc.dart';
 
-@immutable
-sealed class SignUpState {}
+abstract class SignUpState extends Equatable {
+  const SignUpState();
+
+  @override
+  List<Object> get props => [];
+}
 
 final class SignUpInitial extends SignUpState {}
+
+final class SignUpSuccess extends SignUpState {}
+final class SignUpFailure extends SignUpState {}
+final class SignUpProcess extends SignUpState {}
