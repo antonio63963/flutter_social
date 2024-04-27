f5fc56896e085533df3751371856fba4eff5df6e
antonio63963 <shotvideo25@gmail.com>
2024-04-27 11:30:11 +0300

    singIn bloc

--- lib/blocs/sign_in_bloc/sign_in_bloc.dart
+++ lib/blocs/sign_in_bloc/sign_in_bloc.dart
@@ -1,13 +1,32 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:logger/logger.dart';
 import 'package:meta/meta.dart';
+import 'package:user_repository/user_repository.dart';
 
 part 'sign_in_event.dart';
 part 'sign_in_state.dart';
 
+final logger = Logger();
+
 class SignInBloc extends Bloc<SignInEvent, SignInState> {
-  SignInBloc() : super(SignInInitial()) {
-    on<SignInEvent>((event, emit) {
-      // TODO: implement event handler
+  final UserRepository _userRepository;
+
+  SignInBloc({required UserRepository userRepository})
+      : _userRepository = userRepository,
+        super(SignInInitial()) {
+    on<SignInRequired>((event, emit) async {
+      emit(SignInProcess());
+      try {
+        await _userRepository.signIn(event.email, event.password);
+        emit(SignInSuccess());
+      } catch (err) {
+        logger.e(err);
+        emit(SignInFailure());
+      }
+    });
+    on<SignOutRequired>((event, emit) async {
+      await _userRepository.logOut();
     });
   }
 }
--- lib/blocs/sign_in_bloc/sign_in_state.dart
+++ lib/blocs/sign_in_bloc/sign_in_state.dart
@@ -1,6 +1,17 @@
 part of 'sign_in_bloc.dart';
 
-@immutable
-sealed class SignInState {}
+abstract class SignInState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
 final class SignInInitial extends SignInState {}
+
+class SignInSuccess extends SignInState {}
+
+class SignInFailure extends SignInState {
+  final String? message;
+  SignInFailure({this.message});
+}
+
+class SignInProcess extends SignInState {}
