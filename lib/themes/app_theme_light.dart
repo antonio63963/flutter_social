714de11d2546ab71c5cf4c143a53322b182e228c
antonio63963 <shotvideo25@gmail.com>
2024-04-30 13:37:03 +0300

    fixed bug with Large text in input

--- lib/themes/app_theme_light.dart
+++ lib/themes/app_theme_light.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:social/constants/colors.dart';
+import 'package:social/utils/colors.dart';
 
 final appThemeLight = ThemeData(
   colorScheme: myColors,
   textTheme: TextTheme(
-    bodyLarge: TextStyle(
+    // bodyLarge: TextStyle(
+    //   color: myColors.onPrimary,
+    //   fontWeight: FontWeight.w600,
+    //   fontSize: 28,
+    // ),
+    bodyMedium: TextStyle(
       color: myColors.onPrimary,
-      fontWeight: FontWeight.w600,
-      fontSize: 28,
+      fontWeight: FontWeight.w500,
+      fontSize: 16,
+    ),
+    bodySmall: TextStyle(
+      color: myColors.onPrimary,
+      fontSize: 12,
     ),
   ),
 );
